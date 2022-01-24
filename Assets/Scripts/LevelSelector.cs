cf9c4843f3e95669aec197f56f6daf67392d8ef0
Felix Gellner <flink@flinkblog.de>
2022-01-24 12:04:08 +0100

    Level Selector (with some bugs)

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+
+	// public SceneFader fader;
+
+	public void Select(string levelName)
+	{
+		// fader.FadeTo(levelName);
+		SceneManager.LoadScene(levelName);
+	}
+
+}
