522a12d3c6c06503b96378d507b2377cf3318066
Felix Gellner <flink@flinkblog.de>
2022-02-02 12:06:29 +0100

    next level btn at level end, cleanup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
 	public GameObject gameOverUI;
 	public GameObject completeLevelUI;
 
+
+
 	private void Start()
     {
 		GameIsOver = false;
@@ -40,9 +42,6 @@ public class GameManager : MonoBehaviour
 
 	public void WinLevel()
 	{
-		Debug.Log("LEVEL WON!");
-		//PlayerPrefs.SetInt("levelReached", levelToUnlock);
-		//sceneFader.FadeTo(nextLevel);
 		GameIsOver = true;
 		completeLevelUI.SetActive(true);
 	}
